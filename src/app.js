35de0a20068e1a95b89b534ba80613921fa207a5
Vegard-hd <vegard.hd@outlook.com>
2024-02-24 23:22:47 +0100

    Added a global currency variable and function that increments it on click in app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-
-
 //grow trees to generate money $
 
 //Spend money to upgrade and grow more trees to make more $
 
-
-
+let currency = 0;
+$(document).ready(function () {
+	$("#pic1").click(function () {
+		currency += 1;
+		$("#moneyDisplay").text(currency);
+		console.log(currency);
+	});
+});
