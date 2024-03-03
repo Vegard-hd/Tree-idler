713886829664d383dcb58a9e3ed5ce848e74a372
Vegard-hd <vegard.hd@outlook.com>
2024-03-03 18:19:37 +0100

    Adde Upgrade 1 that increases multiplier at the cost of 50 money and a display of the current multiplier with jquery replaceWith().

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@
 
 // Tree takes time to grow?
 
-
 //Global variables
 var money = 0;
 var multiplier = 1;
-
+function mpToPercent(multiplier) {
+	const percentMp = multiplier * 100 + "%";
+	console.log(multiplier);
+	return percentMp;
+}
 //onclick function calls
 $(function () {
 	$("#tree1").click(function () {
@@ -23,14 +26,38 @@ $(function () {
 	$("#tree4").click(function () {
 		gainMoney(multiplier);
 	});
+	//////////////////////
+
+	/////////////
+	//Button 1 spendmoney onclick bind
+	$("#btn1").click(function () {
+		spendMoney();
+	});
 });
 
 function gainMoney(multiplier) {
 	console.log("clicked");
 	money += 1 * multiplier;
 	$("#moneyDisplay").text(money);
+	if (money >= 50) {
+		$("#btn1").removeClass("disabled");
+	}
 }
 
-function spendMoney(currency) {
-	//spends currency to add some sort of multiplier
+//spends currency to add multiplier
+function spendMoney() {
+	console.log("attempted to upgrade!");
+	if (money >= 50) {
+		console.log("upgraded multiplier by +1!!!");
+		money -= 50;
+		multiplier += 1;
+		$("#mp1").replaceWith(`<b
+		id="mp1"
+		class="text-info ms-2 h5"
+		>${mpToPercent(multiplier)}
+	</b>`);
+	}
+	if (money < 50) {
+		$("#btn1").addClass("disabled");
+	}
 }
